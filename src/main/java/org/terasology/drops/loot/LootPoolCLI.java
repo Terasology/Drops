b9f4cda922c9cc2331fec55962ffb9e17558fefe
Michael Pollind <mpollind@gmail.com>
2020-12-30 21:02:02 -0800

    feat(JOML): migrate and update copyright

--- src/main/java/org/terasology/drops/loot/LootPoolCLI.java
+++ src/main/java/org/terasology/drops/loot/LootPoolCLI.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2020 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.drops.loot;
 
 import org.terasology.entitySystem.entity.EntityRef;
@@ -38,24 +25,24 @@ public class LootPoolCLI extends BaseComponentSystem {
 
     /**
      * Quick and dirty solution - fails if core is not set-up
-     *
+     * <p>
      * Since this module is kind of API, it's fine.
+     *
      * @param client
      * @param group
      * @return
      */
     @Command(
-            shortDescription = "Adds a random item from a specified loot pool, defaults to general. DO NOT USE if possible",
-            requiredPermission = PermissionManager.CHEAT_PERMISSION
+        shortDescription = "Adds a random item from a specified loot pool, defaults to general. DO NOT USE if possible",
+        requiredPermission = PermissionManager.CHEAT_PERMISSION
     )
     public String giveRandomLootItem(@Sender EntityRef client,
                                      @CommandParam(value = "group", required = false) String group) {
         Random random = new FastRandom();
         LootableItem randomLoot;
-        if (group != null){
+        if (group != null) {
             randomLoot = lootSystem.getRandomLoot(group);
-        }
-        else {
+        } else {
             randomLoot = lootSystem.getRandomLoot();
         }
         int amount = random.nextInt(randomLoot.minAmount, randomLoot.maxAmount);
