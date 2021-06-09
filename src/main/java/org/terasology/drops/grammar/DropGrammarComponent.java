b7a51c92bc0a0e863f4645ca4a014bf700830706
DarkWeird <foxflameless@gmail.com>
2021-06-09 17:47:53 +0300

    feature(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/drops/grammar/DropGrammarComponent.java
+++ src/main/java/org/terasology/drops/grammar/DropGrammarComponent.java
@@ -1,9 +1,9 @@
-// Copyright 2020 The Terasology Foundation
+// Copyright 2021 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.drops.grammar;
 
 import com.google.common.collect.Maps;
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 import org.terasology.reflection.MappedContainer;
 
 import java.util.List;
@@ -11,11 +11,18 @@ import java.util.Map;
 
 /**
  */
-public final class DropGrammarComponent implements Component {
+public final class DropGrammarComponent implements Component<DropGrammarComponent> {
     public List<String> blockDrops;
     public List<String> itemDrops;
 
     public Map<String, DropDefinition> droppedWithTool = Maps.newLinkedHashMap();
+
+    @Override
+    public void copy(DropGrammarComponent other) {
+        this.blockDrops = other.blockDrops;
+        this.itemDrops = other.blockDrops;
+        this.droppedWithTool = other.droppedWithTool;
+    }
 
     @MappedContainer
     public static class DropDefinition {
--- src/main/java/org/terasology/drops/loot/LootableComponent.java
+++ src/main/java/org/terasology/drops/loot/LootableComponent.java
@@ -1,13 +1,18 @@
-// Copyright 2020 The Terasology Foundation
+// Copyright 2021 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.drops.loot;
 
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 import java.util.List;
 
-public class LootableComponent implements Component {
+public class LootableComponent implements Component<LootableComponent> {
     public List<LootableItem> lootEntries;
+
+    @Override
+    public void copy(LootableComponent other) {
+        this.lootEntries = other.lootEntries;
+    }
 }
 
 
