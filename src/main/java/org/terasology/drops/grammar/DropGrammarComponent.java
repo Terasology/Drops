be255a13839341cfd5df92c44bc5132c4dfbe998
Nail Khanipov <foxflameless@gmail.com>
2021-08-25 14:55:25 +0300

    feature(ecs-gestalt): Migrate Components to gestalt's Components. (#11)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/drops/grammar/DropGrammarComponent.java
+++ src/main/java/org/terasology/drops/grammar/DropGrammarComponent.java
@@ -1,25 +1,43 @@
-// Copyright 2020 The Terasology Foundation
+// Copyright 2021 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.drops.grammar;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 import org.terasology.reflection.MappedContainer;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  */
-public final class DropGrammarComponent implements Component {
+public final class DropGrammarComponent implements Component<DropGrammarComponent> {
     public List<String> blockDrops;
     public List<String> itemDrops;
 
     public Map<String, DropDefinition> droppedWithTool = Maps.newLinkedHashMap();
 
+    @Override
+    public void copyFrom(DropGrammarComponent other) {
+        this.blockDrops = Lists.newArrayList(other.blockDrops);
+        this.itemDrops = Lists.newArrayList(other.blockDrops);
+        this.droppedWithTool = other.droppedWithTool.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().copy()));
+    }
+
     @MappedContainer
     public static class DropDefinition {
         public List<String> blockDrops;
         public List<String> itemDrops;
+
+        /** Create a deep copy of this drop definition. */
+        DropDefinition copy() {
+            DropDefinition copy = new DropDefinition();
+            copy.blockDrops = Lists.newArrayList(this.blockDrops);
+            copy.itemDrops = Lists.newArrayList(this.itemDrops);
+            return copy;
+        }
     }
 }
--- src/main/java/org/terasology/drops/loot/LootableComponent.java
+++ src/main/java/org/terasology/drops/loot/LootableComponent.java
@@ -1,13 +1,29 @@
-// Copyright 2020 The Terasology Foundation
+// Copyright 2021 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.drops.loot;
 
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class LootableComponent implements Component {
+public class LootableComponent implements Component<LootableComponent> {
     public List<LootableItem> lootEntries;
+
+    @Override
+    public void copyFrom(LootableComponent other) {
+        this.lootEntries = other.lootEntries.stream()
+                .map(lootableItem -> {
+                    LootableItem newLootableItem = new LootableItem();
+                    newLootableItem.frequency = lootableItem.frequency;
+                    newLootableItem.prefab = lootableItem.prefab;
+                    newLootableItem.minAmount = lootableItem.minAmount;
+                    newLootableItem.maxAmount = lootableItem.maxAmount;
+                    newLootableItem.group = lootableItem.group;
+                    newLootableItem.item = lootableItem.item;
+                    return newLootableItem;
+                }).collect(Collectors.toList());
+    }
 }
 
 
